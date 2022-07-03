0e2163efae45b7513abd68189b395f4e9c55e41f
bogdan demchenko <bogdandemchenko@gmail.com>
2022-07-03 15:51:27 +0300

    db cred

--- src/restful_crud/core.clj
+++ src/restful_crud/core.clj
@@ -1,5 +1,13 @@
 (ns restful-crud.core
+  (:require [toucan.db :as db]
+            [toucan.models :as models])
   (:gen-class))
+
+(def db-spec
+  [:dbtype "postgres"
+   :dbname "restful-crud"
+   :user "postgres"
+   :password "postgres"])
 
 (defn -main
   "I don't do a whole lot ... yet."
